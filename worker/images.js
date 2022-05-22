a0a886c12d0ac527ece86da9c43092d9215405dd
Hugh O'Brien <git@hughobrien.ie>
2022-05-22 07:58:53 +0100

    inc

--- worker/images.js
+++ worker/images.js
@@ -1,19 +1,19 @@
 async function handleRequest(request) {
   const url = new URL(request.url);
-  const objName = url.pathname.split('/')[2];
-  console.log(`${request.method} object ${objName}: ${request.url}`);
+  const objectName = url.pathname.split('/')[2];
+  console.log(`${request.method} object ${objectName}: ${request.url}`);
 
   switch (request.method) {
     case "GET":
-      const obj = await R2.get(objName)
-      console.log(JSON.stringify(obj));
-      const body = await obj.body;
-      const blob = await obj.blob;
+      const object = await R2.get(objectName)
+      console.log(JSON.stringify(object));
+      const body = await object.body;
+      const blob = await object.blob;
       return new Response(blob, {
         headers: {
-          'content-type': obj.metadata.contentType,
-          'eTag': obj.metadata.eTag,
-          'key': obj.key
+          'content-type': object.metadata.contentType,
+          'eTag': object.metadata.eTag,
+          'key': object.key
         },
       });
   }
@@ -22,3 +22,8 @@ async function handleRequest(request) {
 addEventListener('fetch', (event) => {
   return event.respondWith(handleRequest(event.request));
 });
+
+
+// https://community.cloudflare.com/t/returned-r2-object-in-worked-does-not-include-body/383040/4
+// https://why2point4-images.hughobrien.workers.dev/images/DSC_1787-1k.jpg.webp
+// https://why2point4-images.hughobrien.workers.dev/images/1601.pdf
